6373fa6d6e2dcf14a42b451a1486da4da3582c7a
Spencer <spencer@frozor.io>
2020-01-08 23:32:16 -0500

    Work on any cse website using courselib

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,13 @@ import MsuHrefModule from './lib/module/modules/MsuHrefModule';
 import QuizModule from './lib/module/modules/step/QuizModule';
 import StepModule from './lib/module/modules/step/StepModule';
 import VideoModule from './lib/module/modules/step/VideoModule';
+import PageUtil from './lib/util/PageUtil';
 
-Module.loadModules(
-    new StepModule(),
-    new QuizModule(),
-    new VideoModule(),
-    new MsuHrefModule()
-);
+if (PageUtil.isPageCourseLib) {
+   Module.loadModules(
+       new StepModule(),
+       new QuizModule(),
+       new VideoModule(),
+       new MsuHrefModule()
+   );
+}
